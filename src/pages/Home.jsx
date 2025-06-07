629cf7e0bd60d6b20c46b78052948d6eed8d8a9a
deepak71kr <93750441+deepak71kr@users.noreply.github.com>
2025-06-07 15:25:44 +0530

    adding combo pkg at home pg 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import BeautyBanner from '../components/BeautyBanner'
-import HomeServices from '../components/HomeServices'
 import ComboPackages from '../components/ComboPackages'
+import HomeServices from '../components/HomeServices/index'
 import RatingCard from '../components/RatingCard'
 import Review from '../components/Review'
 
@@ -9,9 +9,9 @@ const Home = () => {
   return (
     <div className="w-full min-w-full overflow-x-hidden">
       <BeautyBanner />
+      <ComboPackages />
       <div className="w-full space-y-4 sm:space-y-6">
         <HomeServices />
-        <ComboPackages />
         <RatingCard />
         <Review />
       </div>
